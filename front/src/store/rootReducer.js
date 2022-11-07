faeccb86c2cea22261f718a0268e5af4b1315ca5
NMMustafina <n.m.mustafina@gmail.com>
2022-11-07 17:49:38 +0600

    #8 Added User store

--- front/src/store/rootReducer.js
+++ front/src/store/rootReducer.js
@@ -1,7 +1,9 @@
 import {combineReducers} from "redux";
+import usersSlice from "./slices/usersSlice";
 import drawerSlice from "./slices/drawerSlice";
 
 const rootReducer = combineReducers({
+    users: usersSlice.reducer,
     drawer: drawerSlice.reducer,
 });
 

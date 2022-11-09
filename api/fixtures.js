458ddd75aa328a972729863405e5b59e30a5970c
Nargiza Sainazarova <nargizasaina@gmail.com>
2022-11-09 14:20:16 +0600

    #15 added Driver fixtures

--- api/fixtures.js
+++ api/fixtures.js
@@ -4,6 +4,7 @@ const config = require('./config');
 
 const User = require('./models/User');
 const Carrier = require('./models/Carrier');
+const Driver = require("./models/Driver");
 
 const run = async () => {
   await mongoose.connect(config.mongo.db);
@@ -30,7 +31,7 @@ const run = async () => {
     avatarImage: 'fixtures/user.png',
   });
 
-  await Carrier.create({
+  const [bahawayCarrier, safewayCargoCarrier, turanExpressCarrier, tumarExpressCarrier] = await Carrier.create({
     title: 'BAHAWAY',
     mc: '1180196',
     dot: '3537967',
@@ -54,6 +55,116 @@ const run = async () => {
     dot: '2638087',
     fedid: '47-4067936',
     description: 'test company #4'
+  });
+
+  const [] = await Driver.create({
+    email: 'umot@gmail.com',
+    name: 'Umot',
+    phoneNumber: '267-535-08-02',
+    companyId: bahawayCarrier._id,
+    status: 'in transit',
+    description: {
+      address: 'US, LA, Avalon c., str. 1, h. 45',
+      DOB: '15.12.1980',
+      info: '',
+      reference: 'Punctual, decent'
+    }
+  }, {
+    email: 'kuba@gmail.com',
+    name: 'Kuba',
+    phoneNumber: '929-377-44-46',
+    companyId: bahawayCarrier._id,
+    status: 'upcoming',
+    description: {
+      address: 'US, LA, Downey c., str. 1, h. 4',
+      DOB: '5.04.1990',
+      info: '',
+      reference: 'Honest, communicative'
+    }
+  }, {
+    email: 'timur@gmail.com',
+    name: 'Timur',
+    phoneNumber: '323-745-44-92',
+    companyId: safewayCargoCarrier._id,
+    status: 'off/home',
+    description: {
+      address: 'US, IL, Chicago c., str. 1, h. 4',
+      DOB: '12.06.1988',
+      info: '',
+      reference: 'Communicative'
+    }
+  }, {
+    email: 'keldibek@gmail.com',
+    name: 'Keldibek',
+    phoneNumber: '513-808-11-30',
+    companyId: safewayCargoCarrier._id,
+    status: 'ready',
+    description: {
+      address: 'US, IL, Chicago c., str. 1, h. 48',
+      DOB: '30.08.1995',
+      info: '',
+      reference: 'Good driver'
+    }
+  } , {
+    email: 'bakdoolot@gmail.com',
+    name: 'Bakdoolot',
+    phoneNumber: '630-670-20-75',
+    companyId: safewayCargoCarrier._id,
+    status: 'n/a',
+    description: {
+      address: 'US, IL, Chicago c., str. 10, h. 48',
+      DOB: '25.07.1993',
+      info: '',
+      reference: 'Professional with all required skills'
+    }
+  } , {
+    email: 'askhat@gmail.com',
+    name: 'Askhat',
+    phoneNumber: '312-684-06-90',
+    companyId: turanExpressCarrier._id,
+    status: 'sleep',
+    description: {
+      address: 'US, NY, New-York c., 5 Avenue, h. 48',
+      DOB: '18.12.2005',
+      info: '',
+      reference: 'Needs to gain more experience'
+    }
+  } , {
+    email: 'mirbek@gmail.com',
+    name: 'Mirbek',
+    phoneNumber: '773-691-36-04',
+    companyId: turanExpressCarrier._id,
+    status: 'in tr/upc',
+    description: {
+      address: 'US, NY, New-York c., str. 56, h. 48',
+      DOB: '3.01.1996',
+      info: '',
+      reference: 'Positive guy'
+    }
+  } , {
+    email: 'bekmurat@gmail.com',
+    name: 'Bekmurat',
+    phoneNumber: '773-946-6304',
+    companyId: tumarExpressCarrier._id,
+    status: 'ready',
+    description: {
+      address: 'US, MS, Gulfport c., str. 4, h. 75, ap. 7',
+      DOB: '22.02.1992',
+      info: '',
+      reference: 'Reliable guy'
+    }
+  } , {
+    email: 'maken@gmail.com',
+    name: 'Maken',
+    phoneNumber: '347-494-13-14',
+    companyId: tumarExpressCarrier._id,
+    status: 'ready',
+    description: {
+      address: 'US, TX, Houston c., str. 45, h. 12, ap. 12',
+      DOB: '5.05.1995',
+      info: '',
+      reference: 'Responsible guy'
+    }
   });
   
   await mongoose.connection.close();

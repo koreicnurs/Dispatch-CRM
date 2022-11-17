619a0f288df99754868729b35699a08b2bcfd9f0
Bermet Abdrakmanova <bkutmanova@gmail.com>
2022-11-18 01:17:52 +0600

    #55 Created auth.feature file for user login

--- tests/step_definitions/steps.js
+++ tests/step_definitions/steps.js
@@ -1,6 +1,21 @@
 const { I } = inject();
-// Add in your custom step files
 
-Given('I have a defined step', () => {
-  // TODO: replace with your own step
+Given('я захожу на страницу {string}', (page) => {
+  I.amOnPage('/' + page);
+  I.wait(4);
+});
+
+Given('я введу данные:', () => {
+  // From "features/auth.feature" {"line":10,"column":3}
+  throw new Error('Not implemented yet');
+});
+
+When('нажимаю на кнопку {string}', () => {
+  // From "features/auth.feature" {"line":13,"column":3}
+  throw new Error('Not implemented yet');
+});
+
+Then('я вижу текст {string}', () => {
+  // From "features/auth.feature" {"line":14,"column":3}
+  throw new Error('Not implemented yet');
 });

838e914d3961878e80d495049f0de24bc88d6763
Timur Kim <kimtimur01@gmail.com>
2022-12-01 22:36:08 +0600

    #134 Added getting all loads and getting of all personal loads of the company

---
+++ api/tests/loads.test.js
@@ -0,0 +1,94 @@
+const mongoose = require('mongoose');
+const request = require('supertest');
+const app = require("../index");
+const Driver = require("../models/Driver");
+
+const TRIPS_STATUSES = ['upcoming', 'transit', 'finished'];
+
+describe('Testing \'loads\' route', () => {
+
+  let user = null;
+  let driver = null;
+  let load = null;
+
+  const getDriver = async () => {
+    driver = await Driver.findOne({email: 'kuba@gmail.com'});
+  };
+
+  const getUser = (email, password) => {
+    it('user should successfully login', async () => {
+      const res = await request(app)
+        .post('/users/sessions')
+        .send({email, password});
+
+      expect(res.statusCode).toBe(200);
+      user = res.body;
+    });
+  };
+
+  const createLoad = () => {
+    getUser('user@gmail.com', 'user');
+
+    it('load should successfully create', async () => {
+      await getDriver();
+
+      const res = await request(app)
+        .post('/loads')
+        .set({Authorization: user.token})
+        .send({
+          loadCode: "T-3K17LSM9",
+          driverId: driver._id.toString(),
+          dispatchId: user._id,
+          price: 2400,
+          miles: 800,
+          rpm: 3,
+          datePU: "12/10/2022",
+          dateDEL: "12/18/2022",
+          timeToPU: "10:30",
+          timeToDel: "05:00",
+          pu: "Pitasdtsburg, PA",
+          del: "Bosadton, MA",
+          comment: "test comment",
+        });
+      // console.log(formData);
+      // console.log(driver);
+      console.log(res.error);
+      // console.log(res.error);
+      // expect(res.statusCode).toBe(200);
+      // expect(res.body.loadCode).toBe('T-3K17LSM9');
+      // expect(res.body.status).toBe('upcoming');
+      load = res.body;
+    });
+  };
+
+  describe('getting of all loads with statuses', () => {
+    getUser('user@gmail.com', 'user');
+
+    TRIPS_STATUSES.forEach(status => {
+      it('should get array of all loads with all statuses', async () => {
+        const res = await request(app)
+          .get('/loads?status=' + status)
+          .set({Authorization: user.token});
+        expect(res.statusCode).toBe(200);
+      });
+    });
+  });
+
+  describe('getting of all personal loads of the company', () => {
+    getUser('bahaway@gmail.com', 'bahaway');
+
+    it('should get of all personal trips of the company', async () => {
+      const res = await request(app)
+        .get('/loads/carrier')
+        .set({Authorization: user.token});
+      expect(res.statusCode).toBe(200);
+    });
+  });
+
+  describe('create a new load', () => {
+    createLoad();
+  });
+
+});
+
+afterAll(() => mongoose.disconnect());

c634ac58d04301aaccf6ea454026e05e77e0cc6d
koreicnurs <koreicnurs@gmail.com>
2022-12-08 14:12:05 +0600

    #154 added variable for trips status, added try catch, refactoring code

--- api/telegramBotSD.js
+++ api/telegramBotSD.js
@@ -7,15 +7,61 @@ const token = "936426396:AAEwbo64h7Nf3lEJ56bW1ZoA3plMlyPl9VQ";
 
 const bot = new TelegramApi(token, {polling: true});
 
+const tripsStatus = {
+    accept: "accept",
+    cancel: "cancel",
+    transit: "transit",
+    finish: "finish",
+    upcoming: "upcoming",
+}
+
 module.exports = driversBot = () => {
+
+    const keyboardTripDelivered = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{
+                    text: 'Finish',
+                    callback_data: tripsStatus.finish
+                }]
+            ],
+        })
+    };
+
+    const keyboardTripAccept = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{
+                    text: 'Accept',
+                    callback_data: tripsStatus.accept
+                },
+                    {
+                        text: 'Cancel',
+                        callback_data: tripsStatus.cancel
+                    }]
+            ],
+        })
+    };
+
+    const keyboardTripTransit = {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{
+                    text: 'In Transit',
+                    callback_data: tripsStatus.transit
+                }]
+            ],
+        })
+    };
+
     bot.on('message', async msg => {
         const text = msg.text.toLowerCase();
         const chatId = msg.chat.id;
 
         if (text === '/start') {
-            const htmlStart = `<b>Supreme Dispatch welcomes you</b> ${msg.from.first_name}
-<b>For continuation, enter your email</b>
-Example => <b>dispatch@gmail.com</b>`
+            const htmlStart = `<b>Компания Supreme Dispatch приветсвует Вас!</b> ${msg.from.first_name}
+<b>Для продолжения работы напишите свою рабочую почту</b>
+Пример => <b>supremh@gmail.com</b>`
             return await bot.sendMessage(chatId, htmlStart, {
                 parse_mode: 'HTML'
             });
@@ -28,14 +74,14 @@ Example => <b>dispatch@gmail.com</b>`
 
                 if (driver) {
                     await Driver.findByIdAndUpdate({_id: driver.id}, {telegramId: msg.from.id});
-                    return await bot.sendMessage(chatId, 'Your telegram connect to your loads with bot\nNow u can see which load u have by using command /load');
+                    return await bot.sendMessage(chatId, 'Вы зарегистрированы в телеграм боте\nТеперь вы можете смотреть, есть ли у вас новый заказ, если введете следущую команду => /load');
                 }
 
                 if (user) {
                     await User.findByIdAndUpdate({_id: user.id}, {telegramId: msg.from.id});
-                    return await bot.sendMessage(chatId, 'Your telegram connect with bot');
+                    return await bot.sendMessage(chatId, 'Вы зарегистрированы в телеграм боте, в этот чат будут приходить оповещения');
                 } else {
-                    return await bot.sendMessage(chatId, 'You are not registered');
+                    return await bot.sendMessage(chatId, 'Вы не зарегистрированы в телеграм боте');
                 }
 
             } catch (e) {
@@ -43,108 +89,88 @@ Example => <b>dispatch@gmail.com</b>`
             }
         }
 
-        const keyboardTripAccept = {
-            reply_markup: JSON.stringify({
-                inline_keyboard: [
-                    [{
-                        text: 'Accept',
-                        callback_data: 'accept'
-                    },
-                        {
-                            text: 'Cancel',
-                            callback_data: 'cancel'
-                        }]
-                ],
-            })
-        };
-
-        const keyboardTripTransit = {
-            reply_markup: JSON.stringify({
-                inline_keyboard: [
-                    [{
-                        text: 'In Transit',
-                        callback_data: 'transit'
-                    }]
-                ],
-            })
-        };
-
         if (text === '/load') {
-            const driver = await Driver.findOne({telegramId: msg.from.id});
+            try {
+                const driver = await Driver.findOne({telegramId: msg.from.id});
 
-            if (!driver) {
-                return await bot.sendMessage(chatId, 'You are not registered');
-            }
-            const load = await Load.find({driverId: driver.id});
-            const upcomingLoad = load[0];
+                if (!driver) {
+                    return await bot.sendMessage(chatId, 'У вас нету заказов по грузам');
+                }
+                const load = await Load.find({driverId: driver.id});
+                const upcomingLoad = load[0];
 
-            if (upcomingLoad && upcomingLoad.status === 'upcoming') {
-                const htmlLoad =
-                    `<b>Load\'s Code</b>: ${upcomingLoad.loadCode}
+                if (upcomingLoad && upcomingLoad.status === tripsStatus.upcoming) {
+                    const htmlLoad =
+                        `<b>Load\'s Code</b>: ${upcomingLoad.loadCode}
 <b>Price</b>: ${upcomingLoad.price}
 <b>Miles</b>: ${upcomingLoad.miles}
 <b>Date Pick Up</b>: ${upcomingLoad.datePU}
 <b>Date Delivery</b>: ${upcomingLoad.dateDEL}
 <b>Location Pick Up from</b>: ${upcomingLoad.pu} => <b>Location Delivery to</b>: ${upcomingLoad.del}`
 
-                await bot.sendMessage(driver.telegramId, htmlLoad, {
-                    parse_mode: 'HTML',
-                });
+                    await bot.sendMessage(driver.telegramId, htmlLoad, {
+                        parse_mode: 'HTML',
+                    });
 
-                await bot.sendMessage(driver.telegramId, 'You can accept and cancel a load ', keyboardTripAccept);
+                    await bot.sendMessage(driver.telegramId, `Вы можете отклонить или принять заказ по грузу ${upcomingLoad.loadCode}\nЧтобы принять нажмите кнопку "Accept"\nЧтобы отклонить нажмите кнопку "Cancel"`, keyboardTripAccept);
+                }
+            } catch (e) {
+                console.log(e);
             }
         }
 
         if (text.match(/(\d)/g)) {
-            const driver = await Driver.findOne({telegramId: msg.from.id});
-            if (driver) {
-                const load = await Load.find({driverId: driver.id});
-                await Load.findByIdAndUpdate({_id: load[0].id}, {timeToPU: text});
-                await bot.sendMessage(driver.telegramId, 'Put the button when u will ready for delivery a load', keyboardTripTransit);
+            try {
+                const driver = await Driver.findOne({telegramId: msg.from.id});
+                if (driver) {
+                    const load = await Load.find({driverId: driver.id});
+                    await Load.findByIdAndUpdate({_id: load[0].id}, {timeToPU: text});
+                    await bot.sendMessage(driver.telegramId, 'Пожалуйста нажмите на кнопку "In Transit" как начнете ехать до места доставки', keyboardTripTransit);
+                }
+            } catch (e) {
+                console.log(e);
             }
         }
     });
 
     bot.on('callback_query', async (msg) => {
-        const keyboardTripDelivered = {
-            reply_markup: JSON.stringify({
-                inline_keyboard: [
-                    [{
-                        text: 'Finish',
-                        callback_data: 'finish'
-                    }]
-                ],
-            })
-        };
+        try {
+            const driver = await Driver.findOne({telegramId: msg.from.id});
+            const load = await Load.findOne({driverId: driver.id});
+            const dispatcherId = await User.findById({_id: load.dispatchId});
 
-        const driver = await Driver.findOne({telegramId: msg.from.id});
-        const load = await Load.findOne({driverId: driver.id});
-        const dispatcherId = await User.findById({_id: load.dispatchId});
-
-        if (msg.data === 'accept') {
-            await bot.sendMessage(driver.telegramId, 'Can you write how much time u need to PU trip');
-        }
-
-        if (msg.data === 'cancel') {
-            if (dispatcherId && dispatcherId.telegramId) {
-                await bot.sendMessage(dispatcherId.telegramId, `Driver refused load - ${load.loadCode}`);
+            if (msg.data === tripsStatus.accept) {
+                await bot.sendMessage(driver.telegramId, 'Пожалуйста напишие время прибытия до груза и загрузки\nПример: 5ч 20мин');
             }
-        }
 
-        if (msg.data === 'transit') {
-
-            if (load.status === 'upcoming') {
-                await Load.findByIdAndUpdate({_id: load.id}, {status: 'transit'});
-                await bot.sendMessage(driver.telegramId, `Пожалуйста нажмите на кнопку FINISH тока, когда как вы доставите груз - ${load.loadCode}`, keyboardTripDelivered);
-            } else {
-                console.log('Вы уже везете груз');
+            if (msg.data === tripsStatus.cancel) {
+                if (dispatcherId && dispatcherId.telegramId) {
+                    await bot.sendMessage(dispatcherId.telegramId, `Водитель отклонил груз - ${load.loadCode}`);
+                    await bot.sendMessage(driver.telegramId, `Вы отказались от груза - ${load.loadCode}\nОповещение диспетчеру было отправлено`);
+                } else {
+                    await bot.sendMessage(driver.telegramId, `Что то пошло не так =( обратитесь в компанию`);
+                }
             }
-        }
 
-        if (msg.data === 'finish') {
-            if (dispatcherId && dispatcherId.telegramId) {
-                await bot.sendMessage(dispatcherId.telegramId, `Водитель - ${driver.name}, доставил груз - ${load.loadCode}`);
+            if (msg.data === tripsStatus.transit) {
+                if (load.status === tripsStatus.upcoming) {
+                    await Load.findByIdAndUpdate({_id: load.id}, {status: 'transit'});
+                    await bot.sendMessage(driver.telegramId, `Пожалуйста нажмите на кнопку FINISH тока, когда как вы доставите груз - ${load.loadCode}`, keyboardTripDelivered);
+                } else {
+                    await bot.sendMessage(driver.telegramId, `Ваш статус груза ${tripsStatus.transit}`);
+                }
             }
+
+            if (msg.data === tripsStatus.finish) {
+                if (dispatcherId && dispatcherId.telegramId) {
+                    await bot.sendMessage(dispatcherId.telegramId, `Водитель - ${driver.name}, доставил груз - ${load.loadCode}`);
+                    await bot.sendMessage(driver.telegramId, `Спасибо за доставленый груз - ${load.loadCode}.\nОповещение о доставке выслана диспетчеру - ${dispatcherId.displayName}`);
+                } else {
+                    await bot.sendMessage(driver.telegramId, `Что то пошло не так =( обратитесь в компанию`);
+                }
+            }
+        } catch (e) {
+            console.log(e);
         }
     });
 };
